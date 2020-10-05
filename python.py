8be62f68d82d6c653376880c890858a22535d440
59090474CS <48551951+59090474CS@users.noreply.github.com>
2020-10-05 23:35:20 +0700

    Update python.py

--- python.py
+++ python.py
@@ -1,97 +1,67 @@
-class Node:
-    def __init__(self,initdata):
-        self.data = initdata
-        self.next = None
-    def getData(self):
-        return self.data
-    def getNext(self):
-        return self.next
-    def setData(self,newdata):
-        self.data = newdata
-    def setNext(self,newnext):
-        self.next = newnext
-class UnorderedList:
-    def __init__(self):
-        self.head = None
-    def printNode(self):
-        current = self.head
-        print('List contains : ',end = '')
-        while current != None:
-            print(current.getData(),end=' ')
-            current = current.getNext()
-    def isEmpty(self):
-        if self.head == None :
-            return True
-        else :
-            return False
-    def add(self,item):
-        tmp = Node(item)
-        if self.head == None :
-            self.head = tmp
-        else :
-            tmp.setNext(self.head)
-            self.head = tmp
-    def size(self): 
-        cur =self.head
-        count = 0
-        while cur != Node:
-            count = count + 1
-            cur =cur.getNext()
-        return count
-    def search(self,item):
-        cur = self.head
-        found = False
-        while cur != None and not found:
-         if cur.getData() == item:
-            found = True
+def isEmpty (self):
+    return self.head == None
+def add(self,item):
+    tmp = Node (item)
+    tmp.setNext(self.head)
+    self.head =tmp
+def size(self):  
+    cur =self.head
+    count = 0 
+    while cur != Node:
+        count += 1
+        cur = cur.getNext()
+    return count
+def search(self,time):
+    cur = self.head
+    found = False
+    while  cur != None and not found:
+        if cur.getData() == itme:
+            found =True
         else:
             cur = cur.getNext()
-        return found
-    def remove(self, item):
-        cur = self.head
-        previous = None
+    return found
+def remove(self,item):
+  cur = self.head
+        pvs  = None
         while True:
             if cur.value == item:
                 break
-            previous, cur = cur, cur.next
-        if previous is None:
+            pvs, cur = cur, cur.next
+        if pvs is None:
             self.head = cur.next
         else:
-            previous.next = cur.next
-    def append(self,item):
-        temp = Node(item)
-        if self.head == Node:
-            self.head = temp
+            pvs.next = cur.next
+def append(self, item):
+    cur = self.head
+    tmp = Node(item)
+    while cur != None:
+      cur = cur.getNext()  
+    cur.setNext(temp)
+def index(self,head):
+    ind = 0
+    cur =self.head
+    found =False
+    while not found:
+        if cur.getData()==item:
+            found = True
+            return ind
         else:
-            cur = self.head
-            while cur.getNext() != None:
-                cur = cur.getNext()
-            cur.setNext(temp)
-    def index(self,item):
-        pos = 0
-        cur = self.head
-        found = False
-        while cur is not None and not found:
-            if cur.getData() == item:
-                found = True
-            else:
-                cur = cur.getNext()
-                pos += 1
-        if not found:
-            raise ValueError('Value not present in the List')
-        return pos
-    def insert(self, item, pos):
-            ind = 0
-            temp = Node(item)
-            previous = None
-            current = self.head
-            if pos == 0:
-                temp.setNext(self.head)
-                self.head = temp
-            else:
-                while ind < pos:
-                    ind += 1
-                    previous = current
-                    current = current.getNext()
-                temp.setNext(current)
-                previous.setNext(temp)
+            cur = cur.getNext()
+            ind+=1
+def insert(self,time,pos):
+    ind = 0
+    temp = Node(item)
+    pvs = Node
+    cur =self.head
+    if pos == 0:
+        tmp.setNext(self.head)
+        self.head =tmp
+    else:
+        while ind < pos:
+            ind += 1
+            pvs = cur
+            cur =cur.getNext()
+        tmp.setNext(cur)
+        pvs.setNext(tmp)
+    
+ 
